7ff3125e177e0116e012476bf504f585a7069b78
divinearcs1-star <divinearcs1@gmail.com>
2026-07-30 16:58:07 +0530

    add images

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   razorpayKey: 'rzp_test_T1bKRGsWtKElGO',
-  apiUrl: 'https://trekone.onrender.com'
+  // apiUrl: 'https://trekone.onrender.com'
+  apiUrl: 'https://srv1845286.hstgr.cloud'
 };
