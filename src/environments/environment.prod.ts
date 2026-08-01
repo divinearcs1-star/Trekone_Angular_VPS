9126fed3356744c4cc596d218c80c6d78bab0fc6
divinearcs1-star <divinearcs1@gmail.com>
2026-08-01 21:31:33 +0530

    countdown

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   razorpayKey: 'rzp_test_T1bKRGsWtKElGO',
-   apiUrl: 'https://trekone-express-vps.onrender.com/api/v1'
+   //apiUrl: 'https://trekone-express-vps.onrender.com/api/v1'
    
-  //apiUrl: 'https://srv1845286.hstgr.cloud/api/v1'
+  apiUrl: 'https://srv1845286.hstgr.cloud/api/v1'
 };
